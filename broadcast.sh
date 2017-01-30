17745345668cdcef252f4b08f6f97a5711216855
Sammy Sammon <anonymous-thing@hotmail.com>
2017-01-30 23:10:45 +1000

    Removed deprecated broadcast script

--- broadcast.sh
+++
@@ -1,8 +0,0 @@
-file="$1"
-
-if [[ $file == *.mkv ]]
-then
-        ffmpeg -re -i '$file' -vf subtitles='$file' -c:v libx264 -c:a aac -strict -2 -f flv rtmp://localhost/stream/index
-else
-        ffmpeg -re -i '$file' -c:v libx264 -c:a aac -strict -2 -f flv rtmp://localhost/stream/index
-fi
